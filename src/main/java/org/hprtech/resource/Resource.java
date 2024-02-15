d377a1e15540eb14445c1b0f76cea2ed1b05fb22
namangarg01 <naman.garg01@nagarro.com>
2024-02-15 13:35:08 +0530

    Authentication using Keycloak Server

---
+++ src/main/java/org/hprtech/resource/Resource.java
@@ -0,0 +1,47 @@
+package org.hprtech.resource;
+
+import io.quarkus.security.identity.SecurityIdentity;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/")
+public class Resource {
+    @Inject
+    SecurityIdentity securityIdentity;
+
+    @GET
+    @RolesAllowed({"student","professor","admin"})
+    @Path("/course_list")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getCourseList(){
+        return Response.ok("Course List: CS ME EE "+securityIdentity.getPrincipal().getName())
+                .build();
+    }
+    @GET
+    @RolesAllowed({"professor","admin"})
+    @Path("/add_course")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response addCourse(){
+        return Response.ok("Added Course: CE").build();
+    }
+    @GET
+    @RolesAllowed("admin")
+    @Path("/update_course")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response updateCourse(){
+        return Response.ok("Updated Course: CE -> CSE").build();
+    }
+
+    @GET
+    @RolesAllowed("admin")
+    @Path("/delete_course")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteCourse(){
+        return Response.ok("Deleted Course: ME").build();
+    }
+}
